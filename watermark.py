5eb95d04300690926c44605b8e3fe9899aba97ad
Kinboise <kinboise@guc1010.top>
2024-12-31 17:05:08 +0800

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -9,25 +9,36 @@ path = os.path.abspath(__file__)
 os.chdir('/'.join(path.split('\\')[:-1]))
 
 enable_shaders = 'false'
-shader_pack = '无光影'
+shader_pack = '无光影包'
+resource_pack = '原版资源包'
 username = ''
 title = ''
 
-# subprocess.run(['xcopy', r'logs\latest.log', r'logs\latest.log.bak', '/Y'])
-if not os.path.exists('config/iris.properties'):
-    enable_shaders = 'false'
-    shader_pack = '无光影'
-else:
+def proc_file_name(file_name):
+    return file_name.strip().replace('.zip', '').replace('_',' ').replace('-',' ').replace('+',' ').replace('[', '').replace(']', '').replace('"', '')
+
+if os.path.exists('options.txt'):
+    with open('options.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            if line.startswith('resourcePacks:'):
+                resource_packs = line
+        for i in resource_packs.split(','):
+            if 'file/' in i:
+                resource_pack = proc_file_name(i.split('/')[-1])
+        if resource_pack != '原版资源包':
+            resource_pack += ' 资源包'
+
+if os.path.exists('config/iris.properties'):
     with open('config/iris.properties', 'r', encoding='utf-8') as f:
         for line in f:
             if line.startswith('enableShaders='):
                 enable_shaders = line.split('=')[1].strip()
             elif line.startswith('shaderPack='):
-                shader_pack = line.split('=')[1].strip().replace('.zip', ' 光影')
-if not os.path.exists('logs/latest.log'):
-    username = ''
-    title = ''
-else:
+                shader_pack = proc_file_name(line.split('=')[1])
+        if shader_pack != '无光影包':
+            shader_pack += ' 光影包'
+
+if os.path.exists('logs/latest.log'):
     with open('logs/latest.log', 'r', encoding='gbk', errors='ignore') as f:
         for line in f:
             if re.match(r'\[\d\d:\d\d:\d\d\] \[Render thread/INFO\]: Setting user: ', line) is not None:
@@ -45,8 +56,9 @@ pngs = sorted([x for x in os.listdir('screenshots') if x.endswith('.png')], key=
 date = time.strftime('%Y%m%d', time.localtime(os.path.getmtime(f'screenshots/{pngs[-1]}')))
 
 if enable_shaders == 'false':
-    shader_pack = '无光影'
+    shader_pack = '无光影包'
 
+print(f'resource_pack: {resource_pack}')
 print(f'shader_pack: {shader_pack}')
 print(f'username: {username}')
 print(f'title: {title}')
@@ -55,6 +67,7 @@ print(f'date: {date}')
 with open('watermarks/temp.svg', 'r', encoding='utf-8') as f:
     svg = f.read()
 
+svg = svg.replace('{{resource_pack}}', resource_pack)
 svg = svg.replace('{{shader_pack}}', shader_pack)
 svg = svg.replace('{{username}}', username)
 svg = svg.replace('{{title}}', title)
